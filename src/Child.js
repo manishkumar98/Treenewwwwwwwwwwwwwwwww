2e73fd2714089b83b30cf0dae0cbc4c8cfa152ec
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-19 11:28:14 +0530

    Latest

--- src/Child.js
+++ src/Child.js
@@ -5,35 +5,20 @@ import ReactDOM from "react-dom";
 import { useEffect, useState } from "react";
 import App from "./App";
 import Parent from "./Parent";
-class Child extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      parent: 0,
-      count: 0
-      // f: 0
-    };
-  }
-  addChild(key, value) {
-    this.setState({ [key]: value + 1 });
-    //this.setState({ f: 1 });
-    console.log(value);
-  }
+export default function Child() {
+  const [count, setCount] = useState(0);
 
-  handleCount() {
-    this.setState((prevState) => ({ count: prevState.count + 1 }));
-  }
-  render() {
-    return (
-      <>
-        <br />
-        <br />
-        <button onClick={() => this.handleCount()}>+</button>
+  const handleCount = () => {
+    setCount(count + 1);
+  };
 
-        <p>Counter : {this.state.count}</p>
-      </>
-    );
-  }
+  return (
+    <>
+      <br />
+      <br />
+      <button onClick={handleCount}>+</button>
+
+      <p>Counter : {count}</p>
+    </>
+  );
 }
-
-export default Child;
